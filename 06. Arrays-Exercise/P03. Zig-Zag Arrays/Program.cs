532d86b806f2e6f386ea5cb1a815f2c65e8f6009
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-04-07 17:31:13 +0300

    Add new Folder

---
+++ 06. Arrays-Exercise/P03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace P03._Zig_Zag_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+         
+           int[] arr1=new int[n];   
+            int[] arr2=new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+           int[] nums=Console.ReadLine().Split(' ').Select(int.Parse).ToArray();  
+                
+                int firstNum=nums[0];   
+                int secondNum=nums[1];  
+
+                if (i%2==0)
+                {
+                    arr1[i] = firstNum;  
+                    arr2[i] = secondNum;
+                }
+                else
+                {
+                    arr2[i]=firstNum;    
+                    arr1[i]=secondNum;
+                }
+            }
+            Console.WriteLine(string.Join(" ",arr1));
+            Console.WriteLine(string.Join(" ",arr2));
+        }
+    }
+}
---
+++ 06. Arrays-Exercise/P04. Array Rotation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace P04._Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr=Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int n=int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                int firstElement = arr[0];
+
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    
+                    arr[j] = arr[j + 1];    
+                }
+               arr[arr.Length-1]=firstElement;
+                
+            }
+            Console.WriteLine(string.Join(" ",arr));
+        }
+    }
+}
