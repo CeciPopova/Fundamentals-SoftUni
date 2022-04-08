44ad72771739614ea2a8a624b15a86b449d9b527
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-04-08 23:23:38 +0300

    Add some Folders

---
+++ 02. Basic Syntax Conditional Statements And Loops-Exercise/09. Padawan Equipment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace P01.Padawan_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double bugdet = double.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+            double lightsabrePrice = double.Parse(Console.ReadLine());
+            double robesPrice = double.Parse(Console.ReadLine());
+            double beltsPrice = double.Parse(Console.ReadLine());
+
+            int countBelts = studentsCount - studentsCount / 6;
+
+
+            double totalPrice = (lightsabrePrice * (Math.Ceiling(studentsCount * 1.1))) + (robesPrice * studentsCount) + (beltsPrice * countBelts);
+
+            if (bugdet >= totalPrice)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalPrice:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($" John will need {Math.Abs(totalPrice - bugdet):f2}lv more.");
+            }
+        }
+    }
+}
