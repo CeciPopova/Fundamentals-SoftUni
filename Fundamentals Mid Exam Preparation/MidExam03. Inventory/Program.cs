6976c0c79b1483b04eb84317ff82416c71f68ddc
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-04-10 18:25:26 +0300

    Add Some Folders

---
+++ Fundamentals Mid Exam Preparation/MidExam03. Inventory/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MidExam03._Inventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = Console.ReadLine().Split(", ").ToList();
+
+            string command = Console.ReadLine();
+
+            while (command!="Craft!")
+            {
+                string[] arr = command.Split(" - ").ToArray();
+                string action = arr[0];
+                string item = arr[1];   
+
+                if (action=="Collect")
+                {
+                    
+                    if (!items.Contains(item))
+                    {
+                        items.Add(item);    
+                    }
+                }
+                else if (action=="Drop")
+                {
+                    
+                    if (items.Contains(item))
+                    {
+                        items.Remove(item);
+                    }
+                }
+                else if (action=="Combine Items")
+                {
+                    string[] oldNew=item.Split(":").ToArray();
+                    string oldItem = oldNew[0];
+                    string newItem = oldNew[1];
+                    if (items.Contains(oldItem))
+                    {
+                        for (int i = 0; i < items.Count; i++)
+                        {
+                            if (items[i]==oldItem)
+                            {
+                                items.Insert(i + 1, newItem);
+                            }
+                        }
+                    }
+                }
+                else if (action=="Renew")
+                {
+                    if (items.Contains(item))
+                    {
+                        items.Remove(item);
+                        items.Add(item);
+                    }
+                }
+
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(String.Join(", ",items));
+        }
+    }
+}
