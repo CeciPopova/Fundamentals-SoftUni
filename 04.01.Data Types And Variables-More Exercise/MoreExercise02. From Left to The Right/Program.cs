b6ddd750e6ecf43b5bb707af8f477b1bda5c1686
Ceci Popova <97601664+Tsetseto@users.noreply.github.com>
2022-04-14 16:31:05 +0300

    Add new Folder

---
+++ 04.01.Data Types And Variables-More Exercise/MoreExercise02. From Left to The Right/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MoreExercise02._From_Left_to_The_Right
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+            long firsSum = 0;
+            long secondSum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                string input = Console.ReadLine();
+                string[] number=input.Split(' ');   
+                long firstNumber=long.Parse(number[0]); 
+                long secondNumber=long.Parse(number[1]);
+
+                if (firstNumber>secondNumber)
+                {
+                    while (firstNumber!=0)
+                    {
+                        firsSum += firstNumber % 10;
+                        firstNumber /= 10;  
+                    }
+                    Console.WriteLine(Math.Abs(firsSum));
+                    firsSum = 0;
+                }
+                else
+                {
+                    while (secondNumber != 0)
+                    {
+                        secondSum += secondNumber % 10;
+                        secondNumber /= 10; 
+                    }
+                    Console.WriteLine(Math.Abs(secondSum));
+                    secondSum = 0;
+                }
+
+            }
+        }
+    }
+}
---
+++ 04.01.Data Types And Variables-More Exercise/MoreExercise03. Floating Equality/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MoreExercise03._Floating_Equality
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal precision = 0.000001M;
+            decimal firsNumber=decimal.Parse(Console.ReadLine());
+            decimal secondNumber=decimal.Parse(Console.ReadLine());
+
+            decimal result=Math.Abs(firsNumber-secondNumber);
+            if (result>=precision)
+            {
+                Console.WriteLine(false);
+            }
+            else
+            {
+                Console.WriteLine(true);
+            }
+        }
+    }
+}
